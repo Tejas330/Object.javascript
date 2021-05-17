b5700467f8ca1dadff7d39eb9c6dad9b3dd8043e
Tejas330 <73092901+Tejas330@users.noreply.github.com>
2021-05-17 13:38:54 +0530

    Update object.js

--- object.js
+++ object.js
@@ -1,20 +1,24 @@
-var cvx = document.createElement('canvas')
-document.body.appendChild(cvx)
-//const cvx = document.querySelector("#cvx");
-            const ctx = cvx.getContext("2d")
-            cvx.height = innerHeight || 600
-            cvx.width = innerWidth || 400
-            class Quadrilateral{
-            constructor(x,y,width,height,color){
-                this.x = x;
-                this.y = y;
-                this.width = width;
-                this.height = height;
-                this.color = color;
+const cvx = document.createElement('canvas');
+document.body.appendChild(cvx);
+const ctx = cvx.getContext("2d");
+cvx.height = innerHeight;
+cvx.width = innerWidth;
+
+
+class Quadrilateral{
+   constructor(x,y,width,height,color){
+           this.x = x;
+           this.y = y;
+           this.width = width;
+           this.height = height;
+           this.color = color;
             }
-            draw(){
-                ctx.beginPath();
-                ctx.rect(this.x,this.y,this.width,this.height );
-                ctx.fillStyle = this.color;
-                ctx.fill()
-            }}
+         draw(){
+            ctx.save();
+            ctx.beginPath();           
+            ctx.rect(this.x,this.y,this.width,this.height );
+            ctx.fillStyle = this.color;
+            ctx.fill();
+            ctx.restore();
+            }
+}
